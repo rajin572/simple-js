edc328aa94b4e7fc16f16cd99507d87d8b4470bb
Din Islam Rajin <dirrazin572@gmail.com>
2022-08-12 21:20:08 +0600

    added recursion.js file

---
+++ recursion.js
@@ -0,0 +1,11 @@
+// recursion : kono ek function er bitor theke oi function ke call korle take recursion bole 
+// recursion is similar to loop
+
+
+function recurction (i){
+    if (i === 1){
+        return 1
+    }
+    return i + recurction(i-1);
+}
+console.log(recurction(10));
