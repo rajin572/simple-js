a67f6953c6d173bf0e46b293b857d8c588869291
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-26 23:50:14 +0600

    create new problem solve

--- getLowestNumberFromarray.js
+++ getLowestNumberFromarray.js
@@ -1,21 +1,18 @@
-function getLowestNumber(number){
-    let lowestNumber = number[0];
-    for (var i = 0; i < number.length; i++){
-        const index = i;
-        const element = number[index];
-        if (element < lowestNumber){
-            lowestNumber = element;
-        }
+function getLowestNumber(number) {
+  let lowestNumber = number[0];
+  for (var i = 0; i < number.length; i++) {
+    const index = i;
+    const element = number[index];
+    if (element < lowestNumber) {
+      lowestNumber = element;
     }
-    return lowestNumber;
+  }
+  return lowestNumber;
 }
 
-var a = [26, 89, 64, 59, 87, 26, 99, 54, 62, 36, 21, 15, 95]
-var getLowest = getLowestNumber (a);
+var a = [26, 89, 64, 59, 87, 26, 99, 54, 62, 36, 21, 15, 95];
+var getLowest = getLowestNumber(a);
 console.log(getLowest);
-
-
-
 
 // function number (num1, num2, num3){
 //     if (num1<num2 && num1<num3){
@@ -29,5 +26,3 @@ console.log(getLowest);
 //     }
 // }
 // number(15,87,2);
-
-
