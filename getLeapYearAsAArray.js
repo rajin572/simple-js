92b004d5ab4d8b0f9e4cbc1c8a298007e5e50b82
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-26 23:47:45 +0600

    create new problem solve

--- getLeapYearAsAArray.js
+++ getLeapYearAsAArray.js
@@ -1,27 +1,23 @@
-function getYears (years){
-    let leapYears = [];
-    for (var i = 0; i < years.length; i++){
-        const index = i;
-        let element = years[index];
+function getYears(years) {
+  let leapYears = [];
+  for (var i = 0; i < years.length; i++) {
+    const index = i;
+    let element = years[index];
 
-        if (element%4 === 0){
-            if (element%100 === 0){
-                if (element%400 === 0){
-                    leapYears.push(element);
-                }
-                else{
-                }
-            }
-            else{
-                leapYears.push(element);
-            }
+    if (element % 4 === 0) {
+      if (element % 100 === 0) {
+        if (element % 400 === 0) {
+          leapYears.push(element);
+        } else {
         }
-        else{
-        }
-        
+      } else {
+        leapYears.push(element);
+      }
+    } else {
     }
-    return leapYears;
+  }
+  return leapYears;
 }
-let yearsInArray = [2000, 2004, 2020, 1258, 1555]
+let yearsInArray = [2000, 2004, 2020, 1258, 1555];
 let getLeapYear = getYears(yearsInArray);
 console.log(getLeapYear);
