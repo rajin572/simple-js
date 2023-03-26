19c26ea724341558b1dc34f62de28a2f4d906aff
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-26 23:41:30 +0600

    create new problem solve

--- fivonacci.js
+++ fivonacci.js
@@ -1,17 +1,15 @@
-function fivonacci(num){
-    let result = [];
-    if (num === 1){
-        result.push(0)
+function fivonacci(num) {
+  let result = [];
+  if (num === 1) {
+    result.push(0);
+  } else if (num === 2) {
+    result.push(0, 1);
+  } else {
+    result.push(0, 1);
+    for (var i = 3; i <= num; i++) {
+      result.push(result[result.length - 1] + result[result.length - 2]);
     }
-    else if (num === 2){
-        result.push(0,1)
-    }
-    else {
-        result.push(0,1)
-        for (var i = 3; i <= num; i++){
-            result.push((result[(result.length)-1]+result[(result.length)-2]))
-        }
-    }
-    return result
+  }
+  return result;
 }
 console.log(fivonacci(15));
