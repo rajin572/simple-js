1c1678376dd7d0e10164d483d67fe89ca73cbbbe
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-26 23:49:31 +0600

    create new problem solve

--- getLowestArray&Object.js
+++ getLowestArray&Object.js
@@ -1,78 +1,67 @@
 // get the lowest number of an array
-function price(array){
-    let lowestPrice = array[0];
-    for (var i = 0; i < array.length; i++){
-        let element = array[i];
-        if (element < lowestPrice){
-            lowestPrice = element;
-        }
+function price(array) {
+  let lowestPrice = array[0];
+  for (var i = 0; i < array.length; i++) {
+    let element = array[i];
+    if (element < lowestPrice) {
+      lowestPrice = element;
     }
-    return lowestPrice;
+  }
+  return lowestPrice;
 }
 
-var prices = [59,264,3615,5,22165];
+var prices = [59, 264, 3615, 5, 22165];
 var getLowestPrice = price(prices);
 console.log(getLowestPrice);
 
-
-
-
-
-
-
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+// // get the lowest age from an array of object
 
-
-
-
-
-// // get the lowest age from an array of object 
-
-function presons (array){
-    let lowestpresons = array[0];
-    for (var i = 0; i < array.length; i++){
-        let element = array[i];
-        if (element.age < lowestpresons.age){
-            lowestpresons = element;
-        }
+function presons(array) {
+  let lowestpresons = array[0];
+  for (var i = 0; i < array.length; i++) {
+    let element = array[i];
+    if (element.age < lowestpresons.age) {
+      lowestpresons = element;
     }
-    return lowestpresons;
+  }
+  return lowestpresons;
 }
 
 var presonss = [
-    {
-        name : "karim",
-        age : 58
-    },
-    {
-        name : "sarim",
-        age : 68
-    },
-    {
-        name : "marim",
-        age : 18
-    },
-    {
-        name : "jarim",
-        age : 28
-    },
-    {
-        name : "tarim",
-        age : 98
-    },
-    {
-        name : "karim",
-        age : 58
-    },
-    {
-        name : "warim",
-        age : 8
-    },
-    {
-        name : "narim",
-        age : 5
-    }
+  {
+    name: "karim",
+    age: 58,
+  },
+  {
+    name: "sarim",
+    age: 68,
+  },
+  {
+    name: "marim",
+    age: 18,
+  },
+  {
+    name: "jarim",
+    age: 28,
+  },
+  {
+    name: "tarim",
+    age: 98,
+  },
+  {
+    name: "karim",
+    age: 58,
+  },
+  {
+    name: "warim",
+    age: 8,
+  },
+  {
+    name: "narim",
+    age: 5,
+  },
 ];
 var getLowestpresons = presons(presonss);
 console.log(getLowestpresons);
